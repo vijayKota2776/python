164cbf9d943ac76d584d371e8cd93d5364c905f7
vijayKota2776 <vijaykota2776@gmail.com>
2024-12-11 23:44:59 +0530

    variable&operator

---
+++ Variable_&_Operator.py
@@ -0,0 +1,26 @@
+length=4
+breadth=4
+side=4
+base=4
+height=4
+radius=4
+base2=4
+pi=3.1444
+
+print(f"Length:{length} \n Breadth:{breadth} \n Side:{side} \n Base:{base} \n Base2:{base2} \n Height:{height} \n Radius:{radius}")
+print()
+print("perimeter of a square :",4*(side))
+print("perimeter of rectangle:",2*(length+breadth))
+print("Area of square:",side**2)
+print("Area of reactangle:",length*breadth)
+print("Area of trangle :",1/2*(base*height))
+print("Area of trapeoid:",1/2*(base+base2)*height)
+print("Area of a circle:",pi*radius**2)
+print("circumference of a circle",2*radius*pi)
+print("Area of cube :",6*height)
+print("volume of cube :",height**3)
+print("Area of sphere:",4*(pi*radius**2))
+print("Volume of sphere:",4/3*(pi*radius**2))
+print("Area of cuboid:",length*breadth*height)
+print("volume of cuboid:",length*base*height)
+print("volume of cone:",1/3*(pi*radius**2*height))
